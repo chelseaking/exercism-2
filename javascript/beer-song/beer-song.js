be5ad80ededc873b4544f2abfd3f580e65676281
Chelsea King <ckingarts@gmail.com>
2016-11-01 17:13:29 -0500

    all the exercises

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,6 @@
+
+  for(var bottles = 100; bottles >= 0; bottles--){
+
+   var verse = bottles + " of beer on the wall, " + bottles + " of beer.\n Take one down, pass it around " + (bottles - 1) + " bottles of beer on the wall.\n";
+console.log(verse);
+  };
